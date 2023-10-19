118d2f31411c59c20372eceebb523b4024ac43e4
Ekamn <144987932+Ekamn@users.noreply.github.com>
2023-10-19 14:33:30 +0800

    fixed errors

--- roll the dice/Main.py
+++ roll the dice/Main.py
@@ -48,17 +48,16 @@ else:
             print()
 
 def displayImages():
-
-
- Image_1 = Image.open("1.jpg")
+ global image_1
+ global ImageTk
+ global lbl_dice1
+ global lbl_dice2
+ global image_2
+ global display_image
  display_image1 = ImageTk.PhotoImage(image_1)
  lbl_dice1.config(image=display_image)
  lbl_dice1.image = display_image1
 
-
-
-
- Image_2 = Image.open("2.jpg")
  display_image2 = ImageTk.PhotoImage(image_2)
  lbl_dice2.config(image=display_image)
  lbl_dice2.image = display_image2
@@ -68,7 +67,7 @@ def displayImages():
 
 # Build the Main Window
 window = Tk()  # Creates a GUI window and gives it a name
-window.title('one arm bandit  - Ekam')  # Window title bar text
+window.title('one arm bandit  - Ekam')  # Window title bar text 
 window.configure(bg='#afeeef')  # Window options
 window.geometry('600x300')  # Sets the window size and position
 
@@ -86,15 +85,36 @@ window.geometry('600x300')  # Sets the window size and position
 lblDice1 = Label(window,text = "",font=("default",14),fg = "black",bg="White" )
 lblDice1.place(x=25, y=25, width=200,height=200)
 
-
 lblDice2 = Label(window,text = "",font=("default",14),fg = "black",bg="White" )
 lblDice2.place(x=25, y=25, width=200,height=200)
-
-
+ 
 lblDice3 = Label(window,text = "",font=("default",14),fg = "black",bg="White" )
 lblDice3.place(x=25, y=25, width=200,height=200)
 
+lblDice4 = Label(window,text = "",font=('default',14),fg = "black",bg="White")
+lblDice4.place(x=25, y=25, width=200,height=200)
 
+lblDice5 = Label (window,text = "" ,font=('default',14),fg = "black",bg="White")
 
+firstWheel = Image.open("1.jpg")
+secondWheel = Image.open("2.jpg")
+thirdWheel = Image.open("3.jpg")
+
+def ChangeThisNameLater():
+    global firstWheel, secondWheel, thirdWheel
+    if((firstWheel == "CHERRY") and (secondWheel != "CHERRY")):
+            win = 2
+    elif((firstWheel == "CHERRY") and (secondWheel == "CHERRY") and (thirdWheel != "CHERRY")):
+            win = 5
+    elif((firstWheel == "CHERRY") and (secondWheel == "CHERRY") and (thirdWheel == "CHERRY")):
+            win = 7
+    elif((firstWheel == "ORANGE") and (secondWheel == "ORANGE") and ((thirdWheel == "ORANGE") or (thirdWheel == "BAR"))):
+            win = 10
+    elif((firstWheel == "PLUM") and (secondWheel == "PLUM") and ((thirdWheel == "PLUM") or (thirdWheel == "BAR"))):
+            win = 14
+    elif((firstWheel == "BELL") and (secondWheel == "BELL") and ((thirdWheel == "BELL") or (thirdWheel == "BAR"))):
+            win = 20
+    elif((firstWheel == "BAR") and (secondWheel == "BAR") and (thirdWheel == "BAR")):
+            win = 25
 
 btn_roll = Button(window)
